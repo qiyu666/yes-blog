75567d4e081ed2dbdce5ce013067e6de9275938d
qiyu666 <3270581350@qq.com>
2026-02-25 19:51:28 +0800

    将打赏改为奶茶图标按钮，鼠标悬停或点击显示收款码

--- src/config.ts
+++ src/config.ts
@@ -58,13 +58,6 @@ export const profileConfig: ProfileConfig = {
 	bio: "热爱技术，热爱生活",
 	links: [
 		{
-			name: "Twitter",
-			icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
-			// You will need to install the corresponding icon set if it's not already included
-			// `pnpm add @iconify-json/<icon-set-name>`
-			url: "https://twitter.com",
-		},
-		{
 			name: "Steam",
 			icon: "fa6-brands:steam",
 			url: "https://store.steampowered.com",
