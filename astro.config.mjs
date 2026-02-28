0f9e7bbb7ac9629705c337ef4fa17972ffafc6a5
qiyu666 <3270581350@qq.com>
2026-02-28 12:37:49 +0800

    更新网站地址为 blog.qiyu666.dpdns.org

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
+	site: "https://blog.qiyu666.dpdns.org/",
 	base: "/",
 	trailingSlash: "always",
 	output: "server",
