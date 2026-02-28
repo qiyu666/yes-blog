4c1783f915e18c5a3d75ace19ed03d33230b641e
qiyu666 <3270581350@qq.com>
2026-02-28 13:07:28 +0800

    修复 Cloudflare 适配器导入

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
+import cloudflare from "@astrojs/cloudflare";
 import { pluginCollapsibleSections } from "@expressive-code/plugin-collapsible-sections";
 import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
 import swup from "@swup/astro";
